5d03f9c890b37110a60119197fb89ea1a1629be3
johny24248 <95579765+johny24248@users.noreply.github.com>
2022-05-19 23:32:23 +0530

    Add files via upload

---
+++ AnimalFun/AnimalFun/Dog.cpp
@@ -0,0 +1,28 @@
+#include "Dog.h"
+#include <iostream>
+using namespace std;
+
+Dog::Dog(string name, double weight, string breed) : Animal(name, weight)
+{
+	this->breed = breed;
+}
+
+string Dog::getBreed() const
+{
+	return breed;
+}
+
+void Dog::digHole() const
+{
+	cout << "I'm digging a hotel!" << endl;
+}
+
+string Dog::makeNoise() const
+{
+	return "woof!";
+}
+
+void Dog::chaseCat() const
+{
+	cout << "Here, kitty kitty!" << endl;
+}
